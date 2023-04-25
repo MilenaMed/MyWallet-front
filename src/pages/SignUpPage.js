d2f4c8f9c9c13d0412c608cf7909900a99c35e68
MilenaMed <milenamfm@gmail.com>
2023-04-24 23:05:21 -0300

    dinamizando a tela Sing In
    *O usuário deve ser capaz de criar uma conta na tela de rota /cadastro.
    *Todos os campos são obrigatórios. Faça validações de acordo com a necessidade que garantam que todos os dados estejam presentes.
    Caso as senhas inseridas nos campos de senha e confirme senha não sejam iguais, a requisição não deve ser feita.

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,20 +1,71 @@
-import { Link } from "react-router-dom"
-import styled from "styled-components"
-import MyWalletLogo from "../components/MyWalletLogo"
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import MyWalletLogo from "../components/MyWalletLogo";
+import { useState } from "react"
+import { useNavigate } from "react-router-dom";
+import axios from "axios"
+
+const URL = `http://localhost:5000/cadastro`
 
 export default function SignUpPage() {
+  const navigate = useNavigate()
+  const [nomeUsuário, setNomeUsuario] = useState("")
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+  const [ConfirmaçãoSenha, setConfirmaçãoSenha] = useState("");
+
+  function passarDados(event) {
+    event.preventDefault()
+    const EmailValido = /\S+@\S+\.\S+/;
+    const dados = { nomeUsuário, email, senha}
+
+    if (nomeUsuário.length > 0 && (email.search(EmailValido) !== -1) && email.length > 11 && senha.length >= 3 && senha === ConfirmaçãoSenha) {
+      const promise = axios.post(URL, dados)
+        promise.then(response => {
+            alert("Usuário cadastrado com sucesso")
+            navigate('/')
+        })
+        promise.catch((err) => {
+            console.log(err)
+        })
+    } else {
+      alert("erro: preencha os dados corretamente")
+    }
+  }
+
   return (
     <SingUpContainer>
       <form>
         <MyWalletLogo />
-        <input placeholder="Nome" type="text" />
-        <input placeholder="E-mail" type="email" />
-        <input placeholder="Senha" type="password" autocomplete="new-password" />
-        <input placeholder="Confirme a senha" type="password" autocomplete="new-password" />
-        <button>Cadastrar</button>
+        <input
+          placeholder="Nome"
+          name="name"
+          type="text"
+          required value={nomeUsuário}
+          onChange={(e) => setNomeUsuario(e.target.value)} />
+        <input placeholder="E-mail"
+          type="email"
+          required value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          placeholder="Senha"
+          type="password"
+          autocomplete="new-password"
+          required value={senha}
+          onChange={(e) => setSenha(e.target.value)} />
+        <input
+          placeholder="Confirme a senha"
+          type="password"
+          autocomplete="new-password"
+          required value={ConfirmaçãoSenha}
+          onChange={(e) => setConfirmaçãoSenha(e.target.value)} />
+        <button onClick={passarDados}>
+          Cadastrar
+        </button>
       </form>
 
-      <Link>
+      <Link to={"/"}>
         Já tem uma conta? Entre agora!
       </Link>
     </SingUpContainer>
